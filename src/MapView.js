468df4e8f25d2e60170f60ba8dea23863dc26e4a
Hamza Idries <idries.hamza@gmail.com>
2023-02-11 17:55:27 +0300

    Changed map start position

--- src/MapView.js
+++ src/MapView.js
@@ -1,8 +1,12 @@
 import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
 
 const KEY = process.env.REACT_APP_GOOGLE_MAP_KEY;
 
 export default function MapView({ data }) {
+    const center = useMemo(() => ({lat: 27, lng: 42.5}), []);
+    const zoom = useMemo(() => 5.5, []);
+
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: KEY
     });
@@ -10,11 +14,8 @@ export default function MapView({ data }) {
     if (!isLoaded) return (<div>Loading...</div>);
     else return (
         <GoogleMap
-            zoom={10}
-            center={{
-                lat: 30,
-                lng: 30
-            }}
+            zoom={zoom}
+            center={center}
             mapContainerClassName="googleMap"
         >
             {
